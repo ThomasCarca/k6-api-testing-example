492fda608e67741f462bdec7baeeefa4940ba733
Thomas Boulbes <thomas.boulbes@manomano.com>
2021-02-22 09:21:29 +0100

    custom rate

--- k6/fast-endpoint.js
+++ k6/fast-endpoint.js
@@ -1,5 +1,9 @@
 import http from 'k6/http';
+import { Rate } from 'k6/metrics';
+
+const errorRate = new Rate('error_rate');
 
 export default function fast() {
-  http.get('http://localhost:3000/fast');
+  const res = http.get('http://localhost:3000/fast');
+  errorRate.add(res.status >= 400);
 }
