17f4300af78fc9b132bc3a96116d9a2a177dafd9
Thomas Boulbes <thomas.boulbes@manomano.com>
2021-03-12 14:28:29 +0100

    thresholds

--- k6/index.js
+++ k6/index.js
@@ -4,10 +4,14 @@ import { Rate } from 'k6/metrics';
 const errorRate = new Rate('error_rate');
 
 export function fast() {
-  const res = http.get('http://localhost:3000/fast');
+  const res = http.get('http://localhost:3000/fast', {
+    tags: { type: 'fast_endpoint' },
+  });
   errorRate.add(res.status >= 400);
 }
 
 export function slow() {
-  http.get('http://localhost:3000/slow');
+  http.get('http://localhost:3000/slow', {
+    tags: { type: 'slow_endpoint' },
+  });
 }
